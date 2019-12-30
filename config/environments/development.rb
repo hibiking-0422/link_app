9570349e28c060ace4d61fbfb569d7b4aa1242ef
root <root@tk2-223-21347.vs.sakura.ne.jp>
2019-12-30 20:24:40 +0900

    devise comp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #devise config
+  config.action_mailer.default_url_options = { host: '160.16.95.101', port: 3000 }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'smtp.gmail.com',
+  :user_name => "linkapp1230@gmail.com",
+  :password => "dkwthnbxkskzcbmn",
+  :authentication => 'login'
+}
 end
