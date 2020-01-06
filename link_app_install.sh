4a4fd431b647d3095721ded91c98a2cf522983c0
hibiking-0422 <50702357+hibiking-0422@users.noreply.github.com>
2020-01-06 22:36:27 +0900

    first  commit

---
+++ link_app_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#install docker
+bash <(curl -s https://raw.githubusercontent.com/hibiking-0422/docker_install/master/golang_install.sh)
+ 
+#link_app install (とりあえず/var以下に配置)
+cd　/var
+git clone https://github.com/hibiking-0422/link_app.git
+
+cd link_app
+
+#build & up
+docker-compose build
+docker-compose up -d
+
+#create database
+docker-compose exec app rails db:create
+docker-compose exec app rails db:migrate
